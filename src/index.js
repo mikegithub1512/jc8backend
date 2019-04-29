349f7a7f540fc50e9b9eb6169147fc4e0e21f143
mikegithub1512 <gabrielmichaeljw@gmail.com>
2019-04-29 11:52:43 +0700

    Added route get to home

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const port = process.env.PORT
 app.use(express.json())
 app.use(userRouter)
 
+app.get('/', (req, res) => {
+    res.send(`<h1>API Running on Port ${port}`)
+})
+
 app.listen(port, () => {
     console.log("Running at ", port);
     
